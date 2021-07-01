8f21ad97083c20e8e79f0f0f1650065017335b1b
Fenomen-Alex <alexfenomen@gmail.com>
2021-07-02 01:56:55 +0300

    Updates

---
+++ precache-manifest.7a59a995c473991b4c92b8dfd4fd03f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f0ae793661107bb51fa593711600b0",
+    "url": "/Memory_Matching_Game/index.html"
+  },
+  {
+    "revision": "14b156aeddd5463e0c8f",
+    "url": "/Memory_Matching_Game/static/css/main.d2100d85.chunk.css"
+  },
+  {
+    "revision": "ec740e3a350ef4832533",
+    "url": "/Memory_Matching_Game/static/js/2.ec7fb5e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Memory_Matching_Game/static/js/2.ec7fb5e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b156aeddd5463e0c8f",
+    "url": "/Memory_Matching_Game/static/js/main.be5219c0.chunk.js"
+  },
+  {
+    "revision": "a07fb89fd20dd3df8315",
+    "url": "/Memory_Matching_Game/static/js/runtime-main.5f8e50bf.js"
+  }
+]);
